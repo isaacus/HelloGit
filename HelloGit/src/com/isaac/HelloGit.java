9ae098cdb5687bc51bd5247a88d4b3b6e061a83e
isaacus <skywalkerlyl@gmail.com>
2014-07-07 14:37:17 -0500

    First GitHub Project

---
+++ HelloGit/src/com/isaac/HelloGit.java
@@ -0,0 +1,13 @@
+package com.isaac;
+
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git");
+	}
+
+}
